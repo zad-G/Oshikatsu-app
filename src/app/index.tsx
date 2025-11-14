6a6ae0b188bf66ca2932bc648f9fbbe189b1042a
Ame <236023242+zad-G@users.noreply.github.com>
2025-11-14 19:40:24 +0900

    Update File Structure

---
+++ src/app/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native'
+
+const Index = (): React.JSX.Element => {
+    return (
+        <View>
+            <Text>義勇さんが凄い・・・</Text>
+            <Text>12345678910</Text>
+        </View>
+    )
+}
+
+export default Index
